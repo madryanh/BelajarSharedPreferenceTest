7aeae915c76125b221c23db722453b0c429e3fdb
madryanh <adryanh90@gmail.com>
2022-04-12 11:26:52 +0700

    first commit

---
+++ app/src/main/java/com/adl/belajarsharedpreferencetest/UjiLab.kt
@@ -0,0 +1,11 @@
+package com.adl.belajarsharedpreferencetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UjiLab : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uji_lab)
+    }
+}
